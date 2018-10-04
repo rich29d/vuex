a94e79a06e6de6e564cacd24907b9a06463a2e15
Richard <talk@richardds.com.br>
2018-10-03 23:13:52 -0300

    store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'My Custom Title',
+    links: [
+      'http://google.com',
+      'http://coursetro.com',
+      'http://youtube.com'
+    ],
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state) => {
+      const removeItem = () => setTimeout(() => {
+        state.links.pop()
+        state.links.length > 0 && removeItem()
+      }, 100);
 
+      removeItem();
+    }
   },
   actions: {
-
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    },
+    removeAll ({commit}) {
+      commit('REMOVE_ALL')
+    }
   }
 })
